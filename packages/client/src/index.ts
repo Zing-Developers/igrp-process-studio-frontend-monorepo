d3e2a8a483334611f8b64a89bf7baac7e62de8e3
NOSI / PM - Carlos Alberto Andrade  Graca <carlosa.graca@nosi.cv>
2025-08-07 20:42:34 -0100

    Add exports for ProcessStudioApiClient and convertToMapOptions

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -1,1 +1,3 @@
 export { createProcessStudioClient } from './services';
+export { ProcessStudioApiClient } from './utils/process-studio-api-client';
+export { convertToMapOptions } from './utils/convert-to-map-options';
